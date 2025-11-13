6280241fb1ceef816cb70cfe5377ca1a6a49de0f
msivakumarreddy <info@joindevops.com>
2025-11-13 10:58:57 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-    source = "../../terraform-aws-roboshop"
+    source = "git::https://github.com/281089-36/terraform-aws-roboshop.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
 }
